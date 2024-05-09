693db21cbc5af9edc771c796a82b270857b11be2
thibemal <malvina.thiberville@univ-rouen.fr>
2024-05-09 19:43:05 +0200

    add index page

--- src/main/java/fr/univrouen/cv24v1/controllers/IndexController.java
+++ src/main/java/fr/univrouen/cv24v1/controllers/IndexController.java
@@ -2,12 +2,14 @@ package fr.univrouen.cv24v1.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class IndexController {
 	
 	@GetMapping("/")
-	public String index() {
-		return "templates/index.html";
+	public ModelAndView index() {
+		ModelAndView mav = new ModelAndView("index");
+		return mav;
 	}
 }
