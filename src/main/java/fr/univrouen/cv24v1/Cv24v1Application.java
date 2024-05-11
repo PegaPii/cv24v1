6c4d7eaf32b64cae378e0d072e90c4022bf80435
lehucrob <61794727+lehucrob@users.noreply.github.com>
2024-05-11 22:33:42 +0200

    supprime des trucs qui servent à rien

--- src/main/java/fr/univrouen/cv24v1/Cv24v1Application.java
+++ src/main/java/fr/univrouen/cv24v1/Cv24v1Application.java
@@ -1,16 +1,13 @@
 package fr.univrouen.cv24v1;
 
-import fr.univrouen.cv24v1.model.testDB;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.transaction.Transactional;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableJpaRepositories("fr.univrouen.cv24v1.*")
@@ -18,9 +15,6 @@ import org.springframework.stereotype.Component;
 @EntityScan("fr.univrouen.cv24v1.*") // entities package name
 public class Cv24v1Application {
 
-
-	@PersistenceContext
-	private EntityManager entityManager;
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(Cv24v1Application.class, args);
